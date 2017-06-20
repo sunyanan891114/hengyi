918a5c781d205d454463f05178b27dff8a1be656
sun.yanan <sun.yanan@rea-group.com>
2017-06-20 20:48:38 +0800

    Fix js reference error

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../dist/static'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: 'static/'
   },
   plugins: [
     new webpack.DefinePlugin({
